875aeed514da78d48c57fa57167e5beb8e134da9
Aleksandra Ponochovna <47608387+AleksandraPonochovna@users.noreply.github.com>
2019-05-31 13:38:01 +0300

    Update HW4.3.java

--- HW4.3.java
+++ HW4.3.java
@@ -1,1 +1,20 @@
+package com.company;
 
+public class Palindrom {
+
+    public static void main(String[] args) {
+        String userString = "Madam, I'm Adam.";
+        if (isPalindrome(userString)) {
+            System.out.print("This string is palindrom!");
+        } else {
+            System.out.print("This string isn't palindrom!");
+        }
+    }
+
+    public static boolean isPalindrome(String text) {
+
+        text = text.replaceAll("[^A-Za-z]", "");
+        String invertedString = new StringBuffer(text).reverse().toString();
+        return text.equalsIgnoreCase(invertedString);
+    }
+}
