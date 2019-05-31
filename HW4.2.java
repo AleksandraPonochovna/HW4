351bc5cbd00d3bf56c6daf9b798f665a775c8f71
Aleksandra Ponochovna <47608387+AleksandraPonochovna@users.noreply.github.com>
2019-05-31 13:38:24 +0300

    Update HW4.2.java

--- HW4.2.java
+++ HW4.2.java
@@ -1,1 +1,25 @@
+package com.company;
 
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class Factorial {
+
+    public static void main(String[] args) {
+        int number;
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter the number: ");
+        number = input.nextInt();
+        System.out.print("Factorial of number: " + factorial(number));
+    }
+
+    public static BigInteger factorial(int number) {
+        BigInteger factorial = new BigInteger("1");
+        BigInteger initialNumber = BigInteger.valueOf(number + 1);
+        BigInteger i;
+        for (i = BigInteger.ONE; !i.equals(initialNumber); i = i.add(BigInteger.ONE)) {
+            factorial = factorial.multiply(i);
+        }
+        return factorial;
+    }
+}
