6fd035666241dcd3aa8d57bb662ec35ad708f962
Aleksandra Ponochovna <47608387+AleksandraPonochovna@users.noreply.github.com>
2019-06-01 15:16:18 +0300

    Update HW4.5.java

--- HW4.5.java
+++ HW4.5.java
@@ -5,28 +5,18 @@ import java.util.Scanner;
 public class SqrtApp {
 
     public static void main(String[] args) {
-        SqrtApp start = new SqrtApp();
         Scanner input = new Scanner(System.in);
         double number = input.nextDouble();
-        double result = start.sqrt(number);
+        double result = sqrt(number);
         System.out.println(result);
     }
 
     public static double sqrt(double x) {
-        double result = 1;
-        double border = Math.max(1, x);
-        double step = 1;
         if (x > 0) {
-            for (double i = 0; i < border; i += step) {
-                if (i * i >= x) {
-                    border = i;
-                    i = border - step;
-                    step -= 0.05;
-                }
-            }
+            x = Math.sqrt(x);
         } else {
             throw new IllegalArgumentException("Expected non-negative number, got " + x);
         }
-        return border;
+        return x;
     }
 }
