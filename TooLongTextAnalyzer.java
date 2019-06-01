bcd0ca33a4a7e849218b20c77fb686da3f0b210f
Aleksandra Ponochovna <47608387+AleksandraPonochovna@users.noreply.github.com>
2019-06-01 15:12:41 +0300

    Update TooLongTextAnalyzer.java

--- TooLongTextAnalyzer.java
+++ TooLongTextAnalyzer.java
@@ -1,9 +1,9 @@
 public class TooLongTextAnalyzer implements TextAnalyzer {
 
-    private int commentMaxLength = 50;
+    private int maxLength = 50;
 
     @Override
     public Label processText(String text) {
-        return text.length() > commentMaxLength ? Label.TOO_LONG : Label.OK;
+        return text.length() > maxLength ? Label.TOO_LONG : Label.OK;
     }
 }
